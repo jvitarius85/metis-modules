ce929695a89300e4a877c7ae6aba6b19aba60561
jvitarius85 <jd@vitarius.org>
2026-04-26 11:18:11 -0500

    Release v1.9.4

--- system/src/Metis/Core/Version.php
+++ system/src/Metis/Core/Version.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Metis\Core;
 
 final class Version {
-    public const CURRENT = '1.9.3.3';
+    public const CURRENT = '1.9.4';
 
     public static function sourcePath(?string $root = null): string {
         $base = $root;
