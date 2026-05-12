86ba381379b5960b699684a4c0133a9a98b88e60
jvitarius85 <jd@vitarius.org>
2026-05-11 20:39:27 -0500

    Release v26.5.8.8

--- system/src/Metis/Core/Version.php
+++ system/src/Metis/Core/Version.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Metis\Core;
 
 final class Version {
-    public const CURRENT = '26.5.8.7';
+    public const CURRENT = '26.5.8.8';
 
     public static function sourcePath(?string $root = null): string {
         $base = $root;
