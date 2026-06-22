8a4504ddf1db1779e3e440e8ef3792c7c3be989a
jvitarius85 <jd@vitarius.org>
2026-06-22 16:00:27 -0500

    Guard media bootstrap compatibility helpers

--- modules/media/bootstrap.php
+++ modules/media/bootstrap.php
@@ -7,12 +7,16 @@ if ( ! defined( 'METIS_ROOT' ) ) {
 
 \Metis\Modules\Media\MediaModule::boot();
 
-function metis_media_find_by_token( string $token ): ?array {
-    $media = \Metis\Modules\Media\MediaLibraryService::findByToken( $token );
-    return is_array( $media ) ? $media : null;
+if ( ! function_exists( 'metis_media_find_by_token' ) ) {
+    function metis_media_find_by_token( string $token ): ?array {
+        $media = \Metis\Modules\Media\MediaLibraryService::findByToken( $token );
+        return is_array( $media ) ? $media : null;
+    }
 }
 
-function metis_media_find_by_filename( string $filename ): ?array {
-    $media = \Metis\Modules\Media\MediaLibraryService::findByFilename( $filename );
-    return is_array( $media ) ? $media : null;
+if ( ! function_exists( 'metis_media_find_by_filename' ) ) {
+    function metis_media_find_by_filename( string $filename ): ?array {
+        $media = \Metis\Modules\Media\MediaLibraryService::findByFilename( $filename );
+        return is_array( $media ) ? $media : null;
+    }
 }
